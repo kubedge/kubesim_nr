375a5a7c54dc37abe3637f3f62172027343b0ed1
Jerome Brette <jbrette@gmail.com>
2018-11-10 19:41:49 +0000

    Update to new base classes
    Change-Id: I94d17f3074117593edb38702e14e9c1e0ab3efc9
    (cherry picked from commit 46a0d2c5f4a9ac061cc22f1030b40d6c67fb4cb1)

--- kubesim_nr/main.go
+++ kubesim_nr/main.go
@@ -27,45 +27,54 @@ import (
 	"time"
 )
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
+const SIM_NAME = "KUBESIM 5G NR"
+const SIM_CONFIG_FILE = "/etc/kubedge/kubesim_conf.yaml"
+const SIM_CONNECTED_UE_FILE = "/etc/kubedge/connected_ue.yaml"
+const SIMPLE_HTTP_SERVER = false
+
+func sim_message(msg string) {
+	log.Printf("%s: %s", SIM_NAME, msg)
+}
+
+func configAPI(w http.ResponseWriter, r *http.Request) {
 	message := r.URL.Path
 	message = strings.TrimPrefix(message, "/")
-	message = "Kubesim LTE Simulator: " + message
+	message = SIM_NAME + " : " + message
 	w.Write([]byte(message))
 }
 
-//arguments
-// arg1=demotype
-// arg2=demovalue
 func main() {
-	log.Printf("%s", "kubesim 5G NR client is running")
+	sim_message("Starting")
+
 	demotype := os.Args[1]
-	demovalue := os.Args[2]
-	log.Printf("demotype=%s, demovalue=%s", demotype, demovalue)
+	log.Printf("%s: demotype=%s", SIM_NAME, demotype)
 
 	var conf config.Configdata
-	conf.Config()
-	log.Printf("kubesim 5G NR client:  product_name=%s, product_type=%s, product_family=%s, product_release=%s, feature_set1=%s, feature_set2=%s",
-		conf.Product_name, conf.Product_type, conf.Product_family, conf.Product_release, conf.Feature_set1, conf.Feature_set2)
+	conf.Config(SIM_NAME, SIM_CONFIG_FILE)
+	log.Printf("%s: product_name=%s, product_type=%s, product_family=%s, product_release=%s, feature_set1=%s, feature_set2=%s",
+		SIM_NAME, conf.Product_name, conf.Product_type, conf.Product_family, conf.Product_release, conf.Feature_set1, conf.Feature_set2)
 
 	var conn connected.Connecteddata
 	var message string
 
-	log.Printf("starting for loop")
-	for {
-		conn.Readconnectvalues()
-		log.Printf("5GC Core server:  connected=%s", conn.Connected)
+	if !SIMPLE_HTTP_SERVER {
+		sim_message("Monitoring UE Connection")
+		for {
+			conn.Readconnectvalues(SIM_NAME, SIM_CONNECTED_UE_FILE)
+			log.Printf("%s: Connected UEs: %s", SIM_NAME, conn.Connected)
 
-		message = strings.Join(conn.Connected, ",")
-		log.Printf("message to send=" + message)
-		client.Client(demotype, message)
+			message = strings.Join(conn.Connected, ",")
+			log.Printf("%s: Message to send: %s", SIM_NAME, message)
+			client.Client(SIM_NAME, demotype, message)
 
-		time.Sleep(15 * time.Second) //every 15 seconds
+			time.Sleep(15 * time.Second) //every 15 seconds
+		}
+	} else {
+		// Simple HTTP Server instead of endless loop
+		http.HandleFunc("/", configAPI)
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			panic(err)
+		}
 	}
-
-	http.HandleFunc("/", sayHello)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
-	}
-	log.Printf("%s", "kubesim 5G NR client is exiting")
+	sim_message("Exiting")
 }
