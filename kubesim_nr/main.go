cc877934f1eec3589818f580f2c934a57f4252db
Jerome Brette <jbrette@gmail.com>
2018-11-10 17:17:27 +0000

    Run gofmt on GO code
    Change-Id: I7409d02147a5f2b7c1f9fc5268049ceb59f778d6
    (cherry picked from commit 8bb716a52327dc66295d281eca49efc6a9cd765b)

--- kubesim_nr/main.go
+++ kubesim_nr/main.go
@@ -15,56 +15,57 @@ limitations under the License.
 */
 
 package main
+
 import (
-  "log"
-  "os"
-  "net/http"
-  "strings"
-  "time"
-  "github.com/kubedge/kubesim_base/grpc/go/kubedge_client"
-  "github.com/kubedge/kubesim_base/config"
-  "github.com/kubedge/kubesim_base/connected"
+	"github.com/kubedge/kubesim_base/config"
+	"github.com/kubedge/kubesim_base/connected"
+	"github.com/kubedge/kubesim_base/grpc/go/kubedge_client"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 )
+
 func sayHello(w http.ResponseWriter, r *http.Request) {
-  message := r.URL.Path
-  message = strings.TrimPrefix(message, "/")
-  message = "Kubesim LTE Simulator: " + message
-  w.Write([]byte(message))
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "Kubesim LTE Simulator: " + message
+	w.Write([]byte(message))
 }
 
 //arguments
 // arg1=demotype
 // arg2=demovalue
 func main() {
-  log.Printf("%s", "kubesim 5G NR client is running")
-  demotype := os.Args[1]
-  demovalue := os.Args[2]
-  log.Printf("demotype=%s, demovalue=%s", demotype, demovalue)
+	log.Printf("%s", "kubesim 5G NR client is running")
+	demotype := os.Args[1]
+	demovalue := os.Args[2]
+	log.Printf("demotype=%s, demovalue=%s", demotype, demovalue)
 
-  var conf config.Configdata
-  conf.Config()
-  log.Printf("kubesim 5G NR client:  product_name=%s, product_type=%s, product_family=%s, product_release=%s, feature_set1=%s, feature_set2=%s",
-             conf.Product_name, conf.Product_type, conf.Product_family, conf.Product_release, conf.Feature_set1, conf.Feature_set2)
+	var conf config.Configdata
+	conf.Config()
+	log.Printf("kubesim 5G NR client:  product_name=%s, product_type=%s, product_family=%s, product_release=%s, feature_set1=%s, feature_set2=%s",
+		conf.Product_name, conf.Product_type, conf.Product_family, conf.Product_release, conf.Feature_set1, conf.Feature_set2)
 
+	var conn connected.Connecteddata
+	var message string
 
-  var conn connected.Connecteddata
-  var message string
+	log.Printf("starting for loop")
+	for {
+		conn.Readconnectvalues()
+		log.Printf("5GC Core server:  connected=%s", conn.Connected)
 
-  log.Printf("starting for loop")
-  for {
-     conn.Readconnectvalues()
-     log.Printf("5GC Core server:  connected=%s", conn.Connected)
+		message = strings.Join(conn.Connected, ",")
+		log.Printf("message to send=" + message)
+		client.Client(demotype, message)
 
-     message = strings.Join(conn.Connected, ",") 
-     log.Printf("message to send=" + message)
-     client.Client(demotype, message)
+		time.Sleep(15 * time.Second) //every 15 seconds
+	}
 
-     time.Sleep(15 * time.Second)  //every 15 seconds
-  }
-
-  http.HandleFunc("/", sayHello)
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-    panic(err)
-  }
-  log.Printf("%s", "kubesim 5G NR client is exiting")
+	http.HandleFunc("/", sayHello)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+	log.Printf("%s", "kubesim 5G NR client is exiting")
 }
